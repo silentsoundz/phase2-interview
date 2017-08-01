8fb4800ca7ab1053dca972371122b37cdb1940f8
Stephani McGrath <silentsoundz1977@gmail.com>
2017-07-31 18:52:49 -0700

    finishes part 2 all queries working

---
+++ part-2/searchByCity.js
@@ -0,0 +1,29 @@
+const clients = require('./clients.json')
+const searchArg = process.argv[2]
+
+const searchByCity = (query) => {
+  let queryLowerCase = query.toLowerCase()
+  const searchResults = []
+
+
+  clients.forEach((client) => {
+    let city = client.city.toLowerCase()
+    if(city.includes(queryLowerCase)) {
+      const newContact = {}
+      newContact.id = client.id
+      newContact.rep_name = client.rep_name
+      newContact.company = client.company
+      newContact.city = client.city
+      newContact.state = client.state
+      searchResults.push(newContact)
+      searchResults.sort((a, b) =>
+      a.city.localeCompare(b.city))
+    }
+  })
+  return searchResults
+}
+
+searchByCity(searchArg)
+
+
+console.log(searchByCity(searchArg));
---
+++ part-2/searchByCompany.js
@@ -0,0 +1,24 @@
+const clients = require('./clients.json')
+const searchArg = process.argv[2]
+
+const searchByCompany = (query) => {
+  let queryLowerCase = query.toLowerCase()
+  const searchResults = []
+  clients.forEach((client) => {
+    let company = client.company.toLowerCase()
+    if(company.startsWith(queryLowerCase)) {
+      const newContact = {}
+      newContact.id = client.id
+      newContact.company = client.company
+      newContact.phone = client.phone
+      searchResults.push(newContact)
+      searchResults.sort((a, b) =>
+    a.company.localeCompare(b.company))
+    }
+  })
+  return searchResults
+}
+
+searchByCompany(searchArg)
+
+console.log(searchByCompany(searchArg));
